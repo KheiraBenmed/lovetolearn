035deceae3bbf024112007c1d40ebf13b74319e1
Kheira Benmeridja <kheira.benmeridja@gmail.com>
2017-08-23 19:01:02 +0200

    footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :lessons
-  resources :reviews, only: [:create]
+  root to: 'lessons#index'
 
+  get 'users/profile', to: 'users#show', as: :user
+
+  resources :lessons, only: [:new, :create, :index, :show, :destroy] do
+    collection do
+      get 'category/:category', to: "lessons#index"
+    end
+    resources :meetings, only: [:create]
+    resources :reviews, only: [:create]
+  end
+  resources :meetings, only: [:show, :destroy, :update]
 end
 
 
-# get 'users/profile', to: 'users#show', as: :user
-
-#   resources :lessons, only: [:new, :create, :index, :show, :destroy] do
-#     collection do
-#       get 'category/:category', to: "lessons#index"
-#     end
-#     resources :meetings, only: [:create]
-#     resources :reviews, only: [:create]
-#   end
-#   resources :meetings, only: [:show, :destroy, :update]
-# end
